a43b8606cb19296dfad8a3884493357f736fabe7
pavasqtcc23 <paulvasqueztecco@gmail.com>
2022-05-14 17:51:33 -0300

    Segundo Proyecto contador de palabras

---
+++ proyecto2/contadorPalabras.py
@@ -0,0 +1,14 @@
+
+
+
+
+'''Preguntamos al usuario en que está pensando. 
+   Cuando se introduce la respuesta, realizará el conteo de palabras en la sentencia
+    e imprimimos en la salida el resultado.'''
+
+
+from tokenize import String
+
+
+pensamiento = input("Hola en que es la pensando?: ")
+pensamiento = String(pensamiento);
